3ed8fa5e6682f333cba402961cf734a05225aadd
Nespoli <alucarddrip@gmail.com>
2026-04-02 08:43:34 -0300

    Loguin e Cadastro

---
+++ resultado_loguin.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Loguin feito com sucesso!</h1>
+    <h2>Você ja pode retornar a pagina inicial</h2>
+    <p><a href="index.html" class="voltar">← Voltar para a página inicial</a></p>
+</body>
+</html>
