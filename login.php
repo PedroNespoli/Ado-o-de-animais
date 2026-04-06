a19282c07e42af6bb5633d79fbf86c7a6cf0df36
Nespoli <alucarddrip@gmail.com>
2026-04-06 08:14:20 -0300

    correção

--- login.php
+++ login.php
@@ -1,20 +1,27 @@
+<?php
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página de Loguin</title>
+    <title>Página de Login</title>
 </head>
+
 <body>
-<h1>Faça seu loguin</h1>
+
+    <h1>Faça seu login</h1>
 
     <label for="email">Email</label>
-<input type="text" id="" name="" required >
+    <input type="text" id="" name="" required>
 
-<label for="cpf">Cpf (somente o números)</label>
-<input type="text" id="" name="" pattern="\d{11}" maxlength="11" required >
+    <label for="cpf">Cpf (somente o números)</label>
+    <input type="text" id="" name="" pattern="\d{11}" maxlength="11" required>
 
-<button type="submit" class="btn" >Fazer Loguin</button>
-
+    <button type="submit" class="btn">Fazer Loguin</button>
 </body>
+
 </html>
