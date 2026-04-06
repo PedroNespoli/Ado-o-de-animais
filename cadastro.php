b981603243d78db5b6f2f2ba0e319842e30749a6
Ana Laura <ana.l.paulo@edu.senai.br>
2026-04-06 13:47:23 -0300

    ajustes-finais

--- cadastro.php
+++ cadastro.php
@@ -1,37 +1,61 @@
+<?php
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página de Cadastro</title>
-    
-    <link rel="stylesheet" href="css/cadastro.css">
+    <title>Cadastro</title>
+    <link rel="stylesheet" href="CSS/login.css">
 </head>
 
 <body>
-    <h1></h1>
 
-<form action="resultado_cadastro.php"></form>
+<header>
+    <div class="logo">🐾 Adote <span>um Amigo</span></div>
+
+    <nav>
+        <a href="index.html">Home</a>
+        <a href="sobre.html">Sobre</a>
+        <a href="adoção.html">Adoção</a>
+        <a href="contato.html">Contato</a>
+    </nav>
+
+    <div>
+        <a href="login.php" class="btn btn-entrar">Entrar</a>
+        <a href="cadastro.php" class="btn btn-cadastrar">Cadastrar</a>
+    </div>
+</header>
+
+<main>
 
 <form method="POST" class="formulario">
 
     <h2>Cadastro 💌</h2>
 
-<label for="name">Nome completo</label>
-<input type="text" id="nome" name="nome" required>  
+    <label>Nome completo</label>
+    <input type="text" name="nome" required>  
 
-<label for="email">Email</label>
-<input type="text" id="" name="" required >
+    <label>Email</label>
+    <input type="email" name="email" required>
 
-<label for="cpf">Cpf (somente o números)</label>
-<input type="text" id="" name="" pattern="\d{11}" maxlength="11" required >
+    <label>CPF (somente números)</label>
+    <input type="text" name="cpf" pattern="\d{11}" maxlength="11" required>
 
-<label for="telefone">Telefone / WhatsApp</label>
-<input type="text" id="" name="" required >
+    <label>Telefone / WhatsApp</label>
+    <input type="text" name="telefone" required>
 
-<button type="subimt" class="btn" > Cadastrar </button>
+    <button type="submit" class="btn">Cadastrar</button>
 
-</form> 
+</form>
+
+</main>
+
+<footer>
+    <p>© 2026 Adote um Amigo - Todos os direitos reservados</p>
+</footer>
 
 </body>
 </html>
--- login.php
+++ login.php
@@ -1,38 +1,53 @@
 <?php
-
-
 ?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página de Login</title>
-    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="CSS/login.css">
 </head>
 
 <body>
 
-    <h1> </h1>
+<header>
+    <div class="logo">🐾 Adote <span>um Amigo</span></div>
 
-    
+    <nav>
+        <a href="index.html">Home</a>
+        <a href="sobre.html">Sobre</a>
+        <a href="adoção.html">Adoção</a>
+        <a href="contato.html">Contato</a>
+    </nav>
 
-<form method="POST" class="formulario">
+    <div>
+        <a href="login.php" class="btn btn-entrar">Entrar</a>
+        <a href="cadastro.php" class="btn btn-cadastrar">Cadastrar</a>
+    </div>
+</header>
 
-    <h2>Faça seu login 💌</h2>
+<main>
+    <form method="POST" class="formulario">
 
-    
-<label for="email">Email</label>
-    <input type="text" id="" name="" required>
+        <h2>Faça seu login 💌</h2>
 
-    <label for="cpf">Cpf (somente o números)</label>
-    <input type="text" id="" name="" pattern="\d{11}" maxlength="11" required>
+        <label>Email</label>
+        <input type="email" name="email" required>
 
-    <button type="submit" class="btn">Fazer Loguin</button>
-    
-</form>
+        <label>CPF (somente números)</label>
+        <input type="text" name="cpf" pattern="\d{11}" maxlength="11" required>
+
+        <button type="submit">Fazer Login</button>
+
+    </form>
+</main>
+
+<footer>
+    <p>© 2026 Adote um Amigo - Todos os direitos reservados</p>
+</footer>
 
 </body>
-
 </html>
